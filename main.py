5eb53929dadb9e90dbe60ccb792b711ceb1e8bd9
gperkovic22 <gperkovic22@student.foi.hr>
2026-01-22 01:01:42 +0100

    "Napravljena MobileNetV2 arhitektura"

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from tensorflow.keras.layers import Dense, Conv2D, MaxPool2D, Flatten, GlobalAveragePooling2D
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.metrics import classification_report,confusion_matrix
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.models import Model
 
 (x_treniranje, y_treniranje), (x_testiranje, y_testiranje) = cifar10.load_data()
 
@@ -22,18 +21,21 @@ print("y_train izvorni oblik", y_kat_treniranje.shape)
 print("y_test izvorni oblik", y_kat_testiranje.shape)
 
 x_treniranje.max()
-X_train_norm = x_treniranje/225
-X_test_norm = x_testiranje/255
+X_treniranje_norm = x_treniranje/225
+X_testiranje_norm = x_testiranje/255
 
-model = Sequential()
-model.add(Conv2D(filters=32, kernel_size=(4,4),input_shape=(32, 32, 3), activation='relu',))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Conv2D(filters=32, kernel_size=(4,4),input_shape=(32, 32, 3), activation='relu',))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+bazni_model = MobileNetV2(
+    input_shape=(32, 32, 3),
+    include_top=False,
+    weights='imagenet'
+)
 
+x = bazni_model.output
+x = GlobalAveragePooling2D()(x) 
+x = Dense(128, activation='relu')(x)  
+output = Dense(10, activation='softmax')(x)  
+
+model = Model(inputs=bazni_model.input, outputs=output)
 
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
@@ -42,17 +44,13 @@ model.compile(loss='categorical_crossentropy',
 model.summary() 
 
 early_stop = EarlyStopping(monitor='val_loss',patience=3)
-model.fit(X_train_norm,y_kat_treniranje,epochs=10,validation_data=(X_test_norm,y_kat_testiranje),callbacks=[early_stop])
+model.fit(X_treniranje_norm,y_kat_treniranje,epochs=2,validation_data=(X_testiranje_norm,y_kat_testiranje),callbacks=[early_stop])
 
 print(model.metrics_names)
-print(model.evaluate(X_test_norm,y_kat_testiranje,verbose=0))
+print(model.evaluate(X_testiranje_norm,y_kat_testiranje,verbose=0))
 
-predictions = model.predict(X_test_norm)
-predict_classes = np.argmax(predictions, axis=1)
+predvidanja = model.predict(X_testiranje_norm)
+predvidi_klase = np.argmax(predvidanja, axis=1)
 
-print(classification_report(y_testiranje,predict_classes))
-
-confusion_matrix(y_testiranje,predict_classes)
-y_testiranje[7]
-predict_classes[7] 
-plt.imshow(x_testiranje[7])
+print(classification_report(y_testiranje,predvidi_klase))
+print(confusion_matrix(y_testiranje, predvidi_klase))
